1dd3f9cacc2bcd92986bf0a8dd79baf5016c6e5f
Reio <reio.poldmaa@voco.ee>
2024-10-28 09:13:09 +0200

    databaas startup & frontend

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/ajad", async (req, res) => {
+const ajad = [
+    {aine_nimi: "Matemaatika", opetaja: "Matemaatika Opetaja", "aeg_algus": "14:15", "aeg_lopp": "15:05", "kuupaev": "28.10.2024"}
+]
 
+app.get("/ajad", async (req, res) => {
+    return res.json({ajad})
 });
 
 app.post("/lisa", async (req, res) => {
+    const {
+        aine_nimi,
+        opetaja,
+        aeg_algus,
+        aeg_lopp,
+        kuupaev
+    } = req.body;
 
 });
 
