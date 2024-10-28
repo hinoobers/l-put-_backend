4b546ceb60fd4181802ac91888e9eb2b542f9b33
Reio <reio.poldmaa@voco.ee>
2024-10-28 09:04:09 +0200

    Initial routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/ajad", async (req, res) => {
+
+});
+
+app.post("/lisa", async (req, res) => {
+
+});
+
+app.post("/login", async (req, res) => {
+
+});
+
+app.listen(3000, () => {
+    console.log("Server kuulab pordi 3000 peal!");
+});
